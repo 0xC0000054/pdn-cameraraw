a68603d5923293e6bf972563d42a790dedba871c
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-08-20 23:17:24 -0600

    Bump copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 // Camera RAW FileType plugin for Paint.NET
 //
 // This software is provided under the MIT License:
-//   Copyright (C) 2015-2018 Nicholas Hayes
+//   Copyright (C) 2015-2018, 2021 Nicholas Hayes
 //
 // See LICENSE.txt for complete licensing and attribution information.
 //
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("null54")]
 [assembly: AssemblyProduct("RawFileType")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018 Nicholas Hayes (aka null54)")]
+[assembly: AssemblyCopyright("Copyright © 2021 Nicholas Hayes (aka null54)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
