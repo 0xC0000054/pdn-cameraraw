aea9143e7a89fc36281be41759ab076b27bae00a
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-01-12 00:33:16 -0700

    Bump copyright year

--- EmptyArray.cs
+++ EmptyArray.cs
@@ -3,7 +3,7 @@
 // Camera RAW FileType plugin for Paint.NET
 //
 // This software is provided under the MIT License:
-//   Copyright (C) 2015-2018, 2021 Nicholas Hayes
+//   Copyright (C) 2015-2018, 2021, 2022 Nicholas Hayes
 //
 // See LICENSE.txt for complete licensing and attribution information.
 //
